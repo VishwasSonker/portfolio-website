7df1218f8a4a05155efe782b137fb160bdafb81a
Vishwas Sonker <vishwasprofessional25@gmail.com>
2026-09-11 19:53:17 +0530

    Add frontend origin

--- backend/main.py
+++ backend/main.py
@@ -71,6 +71,9 @@ app.add_middleware(
 
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+
+        "https://vishwas-sonker-portfolio.netlify.app/",
+        "https://vishwas-sonker-portfolio.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
